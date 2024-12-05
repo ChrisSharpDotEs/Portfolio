357e6976c6fef2b5618ac3e521133bf57c5fb87a
Cristian <82599146+ChrisSharpDotEs@users.noreply.github.com>
2024-12-05 19:59:07 +0000

    Update

---
+++ js/index.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+    let fadeElements = document.querySelectorAll('div.delay-1000');
+    //fadeElements.forEach(item => item.classList.add('opacity-0'));
+});

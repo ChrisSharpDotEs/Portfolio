fd706ab60897aad21ef8c7cb2248c609a23a4cac
Chris_Sharp <82599146+ChrisSharpDotEs@users.noreply.github.com>
2025-03-13 09:43:12 +0000

    Update Index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,3 @@
-const NavbarHighlighter = function () {
-    const navbarItems = document.querySelectorAll(".navbar-item");
-
-    navbarItems.forEach((item) => {
-        item.addEventListener("click", () => {
-            navbarItems.forEach((el) => el.classList.remove("active"));
-            item.classList.add("active");
-        });
-    });
-};
-
 const Swiper = function () {
     const slider = document.querySelector(".slider");
     const slides = document.querySelectorAll(".slide");
@@ -25,37 +14,55 @@ const Swiper = function () {
     nextButton.addEventListener("click", () => showSlide(index + 1));
 };
 
-const MobileViewHandler = function () {
-    const menuButton = document.getElementById("menu-button");
-    const mobileMenu = document.getElementById("mobile-menu");
+class UIManager {
+    constructor(config) {
+        if (UIManager.instance) {
+            return UIManager.instance; 
+        }
+        UIManager.instance = this;
+        this.config = config;
+    }
+    init() {
+        config.forEach(item => {
+            this[item];
+        });
+    }
+    navbarToggler() {
+        const navbarItems = document.querySelectorAll(".navbar-item");
+        navbarItems.forEach((item) => {
+            item.addEventListener("click", () => {
+                navbarItems.forEach((el) => el.classList.remove("active"));
+                item.classList.add("active");
+            });
+        });
+    }
+    menuHandler() {
+        const menuButton = document.getElementById("menu-button");
+        const mobileMenu = document.getElementById("mobile-menu");
 
-    menuButton.addEventListener("click", () => {
-        mobileMenu.classList.toggle("max-h-0");
-        mobileMenu.classList.toggle("max-h-full");
-    });
-};
+        menuButton.addEventListener("click", () => {
+            mobileMenu.classList.toggle("max-h-0");
+            mobileMenu.classList.toggle("max-h-full");
+        });
+    }
+}
+
 class Modal {
     constructor(modalId) {
         this.modal = document.getElementById(modalId);
-        const ahora = new Date();
-        ahora.setTime(ahora.getTime() + (1 * 60 * 1000));
-        const fechaExpiracion = ahora.toUTCString();
+
         if (this.modal) {
             this.cerrarModalBtn = this.modal.querySelector('button[data-modal="close"');
             this.abrirModal = this.abrirModal.bind(this);
             this.cerrarModal = this.cerrarModal.bind(this);
             this.manejarClicFueraModal = this.manejarClicFueraModal.bind(this);
-            document.cookie = `accepted-cookies=false;expires=${fechaExpiracion};path=/;SameSite=Lax;`
         } else {
             throw Error("No se encuentra el modal.");
         }
     }
     initOnLoad() {
-        //TODO corregir
-        if(document.cookie.includes('accepted-cookies')){
-            this.abrirModal();
-            this.handleEvents();
-        }
+        this.abrirModal();
+        this.handleEvents();
     }
     initButtonModal(abrirModalBtnId) {
         this.abrirModalBtn = document.getElementById(abrirModalBtnId);
@@ -79,26 +86,44 @@ class Modal {
     }
 }
 
-function cookieManager() {
-    const tabs = ['tab1-btn', 'tab2-btn', 'tab3-btn'].map(item => document.getElementById(item));
-    const contents = ['tab1-content', 'tab2-content', 'tab3-content'].map(item => document.getElementById(item));
-
-    tabs.forEach((tab, index) => {
-        tab.addEventListener('click', () => {
-            contents.forEach(item => item.classList.add('hidden'));
-            contents[index].classList.remove('hidden')
+class CookieModal extends Modal {
+    constructor(modalId) {
+        super(modalId);
+    }
+    #setCookie() {
+        const ahora = new Date();
+        ahora.setTime(ahora.getTime() + (1 * 60 * 1000));
+        const fechaExpiracion = ahora.toUTCString();
+        document.cookie = `accepted-cookies=false;expires=${fechaExpiracion};path=/;SameSite=Lax;`
+    }
+    init() {
+        if(!document.cookie.includes('accepted-cookies')){
+            this.#setCookie();
+            this.initOnLoad();
+        }
+        this.#cookieManager();
+    }
+    #cookieManager() {
+        const tabs = ['tab1-btn', 'tab2-btn', 'tab3-btn'].map(item => document.getElementById(item));
+        const contents = ['tab1-content', 'tab2-content', 'tab3-content'].map(item => document.getElementById(item));
+    
+        tabs.forEach((tab, index) => {
+            tab.addEventListener('click', () => {
+                contents.forEach(item => item.classList.add('hidden'));
+                contents[index].classList.remove('hidden')
+            });
         });
-    });
-}
+    }
+
+} 
 
 function init() {
-    const miModal = new Modal('miModal');
-    miModal.initOnLoad();
+    const miModal = new CookieModal('miModal');
+    miModal.init();
 
-    NavbarHighlighter();
+    const uiManager = new UIManager(['navbarToggler', 'menuHandler']);
+
     Swiper();
-    MobileViewHandler();
-    cookieManager();
 }
 
 window.addEventListener('load', init);
